e2fbc4831bd998dce4b3a94cea1c3afa27a8ad07
arosas17 <107900256+arosas17@users.noreply.github.com>
2022-10-11 11:16:04 -0500

    Updating Rscript and reorganizing resources

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -14,15 +14,33 @@ summary(linear_regression)
 
 
 # Deliverable 2 -----------------------------------------------------------------------------------------------------------
+
 # reading Suspension_Coil csv files
 suspensionCoil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
 
 #Create the total summary of PSI (Mean, Median, Variance, and Standard Deviation)
 total_summary <- suspensionCoil_table %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI), SD= sd(PSI), .groups = 'keep')
 total_summary
 
+
 #Create the summary by lot of PSI (Mean, Median, Variance, and Standard Deviation)
 lot_summary <- suspensionCoil_table  %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI), Variance=var(PSI), SD= sd(PSI), .groups = 'keep')
 lot_summary 
+
+
 # Deliverable 3 -----------------------------------------------------------------------------------------------------------
+
+# performing t.test 
+# mu = 1500 based on population data according to document received
+t.test(suspensionCoil_table$PSI,mu=1500)
+
+# Lot 1 t.test
+t.test(subset(suspensionCoil_table$PSI, suspensionCoil_table$Manufacturing_Lot == "Lot1"),mu=1500)
+
+# Lot 2 t.test
+t.test(subset(suspensionCoil_table$PSI, suspensionCoil_table$Manufacturing_Lot == "Lot2"),mu=1500)
+
+# Lot 3 t.test
+t.test(subset(suspensionCoil_table$PSI, suspensionCoil_table$Manufacturing_Lot == "Lot3"),mu=1500)
 
